a3bd9faa03dcc19c8189d01b277743724e252023
sathishkumarm0907-ship-it <sathishkumarm0907@gmail.com>
2026-03-11 16:30:38 -0400

    initial commit

---
+++ hello.py
@@ -0,0 +1,23 @@
+#name = input("What is your name? ")
+#print("Hello", name)
+#age = 15
+#if age >= 18:
+ #   print("you are eligible")
+#else:
+ #   print("you are not eligible")
+#experience = 10
+#if experience >=5
+ #       print("Senior")
+#else:
+ #       print("Junior")
+
+Marks = 80
+attendance = 70
+if Marks >= 90 and attendance >=80:
+   print("Distinction")
+else:
+   print("Not Distinction")
+
+
+
+    
---
+++ learn_python.py
@@ -0,0 +1,5 @@
+X=8
+y=8
+print(X is y)
+
+print(type(X))
